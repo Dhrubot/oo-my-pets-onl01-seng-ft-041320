b00696450e773bf5aba9d57b0c2ff23fa8d0df47
Dhrubo Talukder <dhrubo.dh@gmail.com>
2020-04-30 10:10:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
   # code goes here
+  
+  attr_accessor :owner, :mood
+  attr_reader :name
+  
+  @@all = []
+  
+  def initialize(name, owner, mood = "nervous")
+    @name = name
+    @owner = owner
+    @mood = mood
+    @@self << self
+  end
 end
